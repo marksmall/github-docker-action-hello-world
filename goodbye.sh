b17cab10aea0efb9c05f45550aea39103a31dd99
Mark Small <mark.small@astrosat.net>
2023-04-19 20:53:50 +0100

    feat(frontend): Follow tutorial
    Describe why you did what you did
    
    IssueID #

---
+++ goodbye.sh
@@ -0,0 +1,11 @@
+echo "Goodbye"
+
+# git clone --branch 1.35.0 https://github.com/mapbox/tippecanoe.git
+# cd tippecanoe
+# make -j
+# make install
+# tippecanoe --version
+
+# echo "Hello $1"
+# time=$(date)
+# echo "time=$time" >> $GITHUB_OUTPUT
